c1daa901bb16c4aba87a901ef20d4b94c18ad51f
Karencabello <karencabellogonzalez@gmail.com>
2025-01-18 11:02:14 +0100

    update

--- P1/Program1/file_converter.c
+++ P1/Program1/file_converter.c
@@ -27,39 +27,74 @@
 
 //Esta función convierte de binario a texto
 int convert2text(char *path , char *newpath){
-    //abrimos file binario
+    //abrimos file binario modo lectura
     int fbin = open(path, O_RDONLY, 0644); 
     //Comprovamos que ok
-    if(fbin == -1); printf("ERROR"); close(fbin); return 0;
-    //creamos file binario
+    if(fbin == -1); perror("ERROR"); close(fbin); return 0;
+    //creamos file binario modo escritura
     int ftext = open(newpath, O_WRONLY | O_CREAT, 0644);
     //Comprovamos que ok
-    if(ftext == -1); printf("ERROR"); close(ftext); return 0;
+    if(ftext == -1); perror("ERROR"); close(ftext); return 0;
 
-    //quan \0 cambiem per , i escrivim  
+    //fata acabar
+    unsigned char buffer;
+    while(read(fbin, &buffer, 0644) > 1){
+        //quan \0 cambiem per , i escrivim  
+        if(buffer == '\0'){
+            write(ftext, ",", 0644);
+        }
+        else{
+            write(ftext, &buffer, 0644);
+        }
+    }  
     //CERRAR
+    close(fbin);
+    close(ftext);
+    return 1;
 }
 
 //Esta función convierte de text a binario
 int convert2bin(char *path, char *newpath){
-    //abrimos file texto
+    //abrimos file texto modo lectura
     int ftext = open(path, O_RDONLY, 0644);
     //Comprovamos que ok 
-    if(ftext == -1); printf("ERROR"); return 0;
-    //creamos file binario
+    if(ftext == -1); perror("ERROR"); return 0;
+    //creamos file binario modo escritura
     int fbin = open( newpath, O_WRONLY | O_CREAT, 0644);
     //Comprovamos que ok 
-    if(fbin == -1); printf("ERROR"); close(fbin); return 0;
+    if(fbin == -1); perror("ERROR"); close(fbin); return 0;
 
     //Variables
     char buffer[1024];
+    int i = 0;
     int bytes_leidos;
-
-    //
-
-    //atoi text a enter
-    //quan , cambiem per \0 i escrivim 
+    char number[16]; //acumular numeros com text
+    int num_index = 0;
+    while((bytes_leidos = read(ftext, buffer, sizeof(buffer))) > 0){
+        for(i = 0; i < bytes_leidos; i++){
+            if(buffer[i] == ','){ //quan , cambiem per \0 i escrivim 
+                //convertimos a binari
+                number[num_index] = '\0'; // Acabamos string
+                int value = atoi(number); //atoi text a enter
+                write(fbin, &value, sizeof(int)); //Escrivimos entero en bibnario
+                num_index = 0; //Reiniciar índice para el siguiente número
+            }
+            else{
+                //Acumulemos al número actual
+                number[num_index++] = buffer[i];
+                if(num_index >= sizeof(number) - 1){
+                    fprint('Número demasiado grande');
+                    close(ftext);
+                    close(fbin);
+                    return 0;
+                }
+            }
+        }
+    }
     //CERRAR
+    close(ftext);
+    close(fbin);
+    return 1;
 }
 
 int main(int argc, char* argv[]){
@@ -71,6 +106,21 @@ int main(int argc, char* argv[]){
 
     int conversion;
     //Miramos tipo y hacemos la acción
-    if(type == '-binary'); conversion = convert2text(&path , &newpath);
-    if(type == '-text'); conversion = convert2bin(&path , &newpath);
+    if(strcmp(type,'-binary')){
+        conversion = convert2text(&path , &newpath);
+    }
+    if(strcmp(type,'-text')){
+        conversion = convert2bin(&path , &newpath);
+    } 
+    else{
+        printf("Error: %s no es un tipo válido\n", type);
+    }
+
+    if(conversion == 1){
+        printf("Conversión exitosa.\n");
+    }
+    else{
+        printf("Error durante la conversión.\n");
+    }
+    return 0;
 }
