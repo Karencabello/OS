d5e9f21600754fd5a2edf8f90da234ce774567ec
Karencabello <karencabellogonzalez@gmail.com>
2025-01-31 13:17:40 +0100

    father

--- directoryExplorer.c
+++ directoryExplorer.c
@@ -28,23 +28,27 @@ that directly or indirectly use a system call (i.e. fwrite).
 #include <dirent.h>
 // Usage: /.directoryExplorer directory
 
-int son(){
+void son(){
     //Funciones que hace el hijo
         //Abrimos file
         //Leemos file como int
         //sumamos int
         //Escribimos en pipe
-        //exit
+        //exit!!!!!!!!!
 
 }
 
-int father(){
-    int fd[2];
-    pipe(fd);
-    //Father
-        //read pipe
-        //printf
+void father(int fd){
+    //Variables
+    char buffer[1024]; //Leer contenido de la pipe
+    ssize_t bytesRead; 
+    //read pipe hasta que no haya más datos
+    while((bytesRead == read(fd, buffer, sizeof(buffer))) > 0){
+        //Imprimimos lo que hemos leido
+        printf("%s\0", buffer);
+    }
         //Cuando todos acaben --> exit
+        exit(0);
 }
 
 
@@ -52,12 +56,25 @@ int father(){
 
 
 int main(int argc, char* argv[]) {
+    //Gesstion de errores argumentos
+    if (argc < 2) {
+        perror('Argumentos incorrectos');
+        return 1;
+    }
     // 1. Abrir el directorio
     // Coge directorio argumentos CLI
     char * filename = argv[1];
     
     // Abre el directorio especificado en filename.
     DIR* d = opendir(filename);
+    //Gestión de errores al abrir el directorio
+    if (d == NULL) {
+        perror("No se pudo abrir el directorio");
+        return 1;
+    }
+    //Creamos la pipe
+    int fd[2];
+    pipe(fd);
 
     // Declara un puntero para leer los archivos del directorio.
     struct dirent* fileInDir;
@@ -65,13 +82,39 @@ int main(int argc, char* argv[]) {
     // Itera sobre los elementos del directorio.
     while ((fileInDir = readdir(d)) != NULL) {
         //List files --> PRINT
+        printf('List of files in %c:\n', filename);
+        printf("%s\n", fileInDir->d_name);
         //if dir == .dat
-        //Creamos hijo
-        //llamar a funcion son
-        //exit son
+        if (strstr(fileInDir->d_name, ".dat") != NULL) {
+            //Usamos strstr para buscar el .dat en el nombre del archivo y mientras sea diferente a NULL,
+            //es decir, que no se encuentran más .dat , ejecutamos el codigo.
+
+            //Creamos hijo
+            pid_t pid = fork(); //Cada vez que encuentre un archivo .dat llamara a fork y por lo tanto, creara un hijo
+            if (pid == 0) {
+                close(fd[0]); //Cerramos pipe lectura antes de ejutar son
+                //TAMBIEN SE PUEDE HACER DENTRO DE SON NS Q ES MEJOR
+                //llamar a funcion son
+                son(fileInDir->d_name, fd[1]); //Ajustar atributos y tal
+                //Exit son en la función son
+            }
+            //El padre no hace nada y continua con la ejecución
+        }
+        
         //llamar a la funcion padre
         //exit padre
         
 
-    }   
+    }  
+    //Cerramos el directorio
+    closedir(d);
+    //Como ya han acabado todos los hijos empecemos con el padre
+    //Cerramos pipe escritura
+    close(fd[1]);
+    //Llamamos a la función padre
+    padre(fd[0]);
+    //Cerramos pipe lectura
+    close(fd[0]);
+    //Acabamos
+    return 0;
 }
