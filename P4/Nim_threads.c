9070cef517141fc785ae6434867c7fd440e488c5
Karencabello <karencabellogonzalez@gmail.com>
2025-02-27 16:54:51 +0100

    x

---
+++ P4/Nim_threads.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+//Handle_turn i handle_victory 
+
+int main(int argc, char* argv[]){
+    // read number of tokens from CLI and write to binary file named game.dat
+    int tokens = atoi(argv[1]); // from string to integer
+    fd = open("game.dat", O_CREAT | O_RDWR, 0644);
+    write(fd, &tokens, sizeof(int));
+
+    // game
+}
---
+++ P4/apunts.c
@@ -0,0 +1,13 @@
+//Game.dat --> number of tokens --> lab3
+//Player A and player B 3 threads each
+//Thread --> which player it belongs, and number of tokens to remove
+// **** To make it simpler, for each player there will be a thread that decreases the counter by 1, another by 2, and another one by 3.
+
+//Starts player A
+    // 1. thre threads lock file
+    // 2. But only one of them (which, from our perspective, will be random) will succeed, and will decrease the number of tokens in game.dat according to its value.
+    // 3. pass turn --> Make proper use of synchronization to ensure that only one thread of one player modifies the file at a time. 
+//Game ends --> ll threads should be woken up and declare whether player A or B has won. Then, make sure that the program ends correctly.
+
+    // CODE GIVEN --> requires all the synchronization mechanisms to make the game work as described.
+//IMPORTANT --> make sure to avoid active waiting and starvation (HINT: use condition variables). 
