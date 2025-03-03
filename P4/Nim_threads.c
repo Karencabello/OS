5c6c24b4b78aad8ebdd50762c0a6f0211cadbf67
sunflower2732 <167934829+sunflower2732@users.noreply.github.com>
2025-03-03 20:52:00 +0000

    Update Nim_threads.c

--- P4/Nim_threads.c
+++ P4/Nim_threads.c
@@ -8,69 +8,51 @@
 
 #define NUM_THREADS 3
 
-int counter; // Contador compartido entre los jugadores
-int turn = 1; // Indica de quién es el turno: 1 --> A ; 0 --> B
+int counter;
+int turn = 1; // 1 --> A ; 0 --> B
 int winner = -1; // -1: No winner yet, 1 --> A, 0 --> B
-int fd; 
-pthread_mutex_t lock; // Mutex para la sincronización de hilos
-pthread_cond_t condA, condB; // Variables de condición para el control de turnos
-int game_over = 0; // Indica el fin de juego 
+int fd;
+pthread_mutex_t lock;
+pthread_cond_t condA, condB;
 
 void *thread_player_A(void *arg) {
-    
     usleep(10000);
     int decrement = *((int *)arg);
     free(arg);
     
     while (1) {
-        pthread_mutex_lock(&lock); // Bloquea el mutex para acceso exclusivo
-
-        /*if (counter <= 0) { // Verifica si el contador llegó a 0 para winner
+        pthread_mutex_lock(&lock);
+        
+        if (counter <= 0) {
             if (winner == 0)
                 printf("THREAD: A.%d. I announce Player B is the winner\n", decrement);
             else if (winner == 1)
                 printf("THREAD: A.%d. I announce Player A is the winner\n", decrement);
             pthread_mutex_unlock(&lock);
             return NULL;
-        }*/
-
-        if(game_over){ // Verifica si el juego ha terminado
-            pthread_mutex_unlock(&lock);
-            return NULL;
         }
         
-        while (turn == 0 && !game_over){ // Wait para consumir menos mientras no es el turno 
+        while (turn == 0 && counter > 0)
             pthread_cond_wait(&condA, &lock);
-        }
 
-        if (game_over){
-            pthread_mutex_unlock(&lock);
-            return NULL;
-        }
-
-        if (counter > 0 && turn == 1 && counter >= decrement) { // Realiazamos movimiento y actualizamos game.dat 
+        if (counter > 0 && turn == 1 && counter >= decrement) {
             printf("Player A decreasing %d (before: %d)\n", decrement, counter);
             counter -= decrement;
-            lseek(fd, 0, SEEK_SET);
-            write(fd, &counter, sizeof(int)); // update game.dat
             printf("COUNTER after A: %d\n", counter);
             
-            if (counter <= 0) { // Gestionamos ganador 
+            if (counter <= 0) {
                 winner = 1;
-                game_over = 1; // Indica que el juego ha terminado
                 pthread_cond_broadcast(&condA);
                 pthread_cond_broadcast(&condB);
             }
-            else{
-                turn = 0; // Cambiamos el turno 
-                pthread_cond_signal(&condB); // Cambiamos turno llamando al jugador B
+            else
+            {
+                pthread_cond_signal(&condB);
             }
-            printf("Player A finished move, turn is now: %d\n", turn);
-
+            turn = 0;
         }
         
-        pthread_mutex_unlock(&lock); // Desbloqueamos mutex
-        if(game_over) break;
+        pthread_mutex_unlock(&lock);
     }
     return NULL;
 }
@@ -83,47 +65,37 @@ void *thread_player_B(void *arg) {
     while (1) {
         pthread_mutex_lock(&lock);
         
-        /*if (counter <= 0) {
+        if (counter <= 0) {
             if (winner == 0)
-                printf("THREAD: B.%d. I announce Player B is the winner\n", decrement);
+                printf("THREAD: A.%d. I announce Player B is the winner\n", decrement);
             else if (winner == 1)
-                printf("THREAD: B.%d. I announce Player A is the winner\n", decrement);            
-            pthread_mutex_unlock(&lock);
-            return NULL;
-        }*/
-
-        if(game_over){ // Verifica si el juego ha terminado
+                printf("THREAD: A.%d. I announce Player A is the winner\n", decrement);            
             pthread_mutex_unlock(&lock);
             return NULL;
         }
         
-        while (turn == 1 && !game_over){
+        while (turn == 1 && counter > 0)
             pthread_cond_wait(&condB, &lock);
-        }
 
         if (counter > 0 && turn == 0 && counter >= decrement) {
             printf("Player B decreasing %d (before: %d)\n", decrement, counter);
             counter -= decrement;
-            lseek(fd, 0, SEEK_SET);
-            write(fd, &counter, sizeof(int)); // update game.dat
             printf("COUNTER after B: %d\n", counter);
             
             if (counter <= 0) {
                 winner = 0;
-                game_over = 1;
                 pthread_cond_broadcast(&condA);
                 pthread_cond_broadcast(&condB);
             }
-            else{
+            else
+            {
                 turn = 1;
                 pthread_cond_signal(&condA);
             }
-            printf("Player B finished move, turn is now: %d\n", turn);
-
+            
         }
         
         pthread_mutex_unlock(&lock);
-        if(game_over) break;
     }
     return NULL;
 }
@@ -148,38 +120,28 @@ int main(int argc, char* argv[]){
     
     pthread_t thread_A[NUM_THREADS], thread_B[NUM_THREADS];
     
-    // Creamos threads A
     for (int i = 0; i < NUM_THREADS; i++) {
         int *arg = malloc(sizeof(int));
         *arg = i + 1;
         pthread_create(&thread_A[i], NULL, thread_player_A, arg);
     }
     
-    // Creamos threads B
     for (int i = 0; i < NUM_THREADS; i++) {
         int *arg = malloc(sizeof(int));
         *arg = i + 1;
         pthread_create(&thread_B[i], NULL, thread_player_B, arg);
     }
     
-    // Esperamos a que los threads se creen
     for (int i = 0; i < NUM_THREADS; i++) {
         pthread_join(thread_A[i], NULL);
         pthread_join(thread_B[i], NULL);
     }
-
-    // Anunciamos ganador
-    if (winner == 0)
-        printf("Game Over! Player B wins!\n");
-    else if (winner == 1)
-        printf("Game Over! Player A wins!\n");
     
-    // Destruimos mutex y variables de condición
+    printf("Game Over! Player %c wins!\n", winner == 1 ? 'A' : 'B');
+    
     pthread_mutex_destroy(&lock);
     pthread_cond_destroy(&condA);
     pthread_cond_destroy(&condB);
-
-    close(fd);
     
     return 0;
 }
